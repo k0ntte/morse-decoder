71d8a285eba0bb2b070fe3d1fd81cd825eba3ed6
k0ntte <bystrov_igor@bk.ru>
2023-06-08 00:35:25 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -39,8 +39,47 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let rez = "";
+    let strTwo = "";
+    let massMorze = "";
+    const masStr = [];
+    let k = true;
+   for(let i =0;i<expr.length;i = i+10){
+    masStr.push(expr.slice(i,i+10));
+   }
+
+   for (const iterator of masStr) {
+    for(let i = 0; i<iterator.length;i = i+2){
+        strTwo += iterator.slice(i,i+2);
+        if(strTwo == "10"){
+            massMorze += ".";
+        }
+        if(strTwo == "11"){
+            massMorze += "-";
+        }
+        if(strTwo == "**"){
+            rez += " ";
+            strTwo = "";
+            massMorze = "";
+            k = false;
+            break;
+        }
+
+        strTwo = "";
+        k = true;
+
+    }
+    if(k){
+        rez += MORSE_TABLE[massMorze];
+        massMorze = "";
+    }
+    
+   }
+   return rez;
 }
 
 module.exports = {
     decode
 }
+
+    
